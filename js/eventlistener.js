f6427bbdfedbf4cdd33fbd009cbe53a52abae480
Markus Gede <markusgede93@gmail.com>
2023-10-19 11:45:13 +0200

    feedback 3

--- js/eventlistener.js
+++ js/eventlistener.js
@@ -10,9 +10,6 @@ function runEventlisteners() {
         if (e.keyCode == 37) {
             left(true);
         }
-        if (e.keyCode == 38) {
-            up(true);
-        }
         if (e.keyCode == 40) {
             keyboard.DOWN = true;
             resetTimer();
@@ -47,9 +44,6 @@ function runEventlisteners() {
         }
         if (e.keyCode == 37) {
             left(false);
-        }
-        if (e.keyCode == 38) {
-            up(false);
         }
         if (e.keyCode == 40) {
             keyboard.DOWN = false;
